80090445c080365932bd5ab3619fc470cd5d8e05
Kanta Demizu <demizu.1203@gmail.com>
2019-11-18 17:42:05 +0900

    :+1: Works LED Matrix

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
-from flask import Flask, request
-# from controller import controller
+import os
+from flask import Flask, request, send_from_directory, flash, render_template
+from werkzeug.utils import secure_filename, redirect
+from controller import *
+import subprocess
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
-def show():
-    if request.headers['Content-Type'] == 'application/json':
-        json = request.get_json()
-        print(json['image'])
-        # controller.showImage(json['image'])
-    return request.get_data()
+@app.route('/', methods=['GET', 'POST'])
+def upload_image():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            image_path = app.config['UPLOAD_FOLDER'] + '/' + filename
+            #controller.showImage(image_path)
+            subprocess.run(['/usr/local/bin/led-image-viewer', image_path, '--led-no-hardware-pulse'])
+            return redirect(request.url)
+    return render_template('index.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
